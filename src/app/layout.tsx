3a77bad33392ec0fc70f324b4ebd5a52b7ddc9e6
Austris Cirulnieks <austris.cirulnieks@gmail.com>
2025-07-09 11:55:44 -0500

    Sets up initial project structure and tooling
    Adds configuration files for formatting, linting, and other tooling.
    Includes initial Next.js configuration and project layout.

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import "@/styles/globals.css"
+
+import { LucideHome } from "lucide-react"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Fillout take home",
+  description: "Fillout take home assignment",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className="m-0 p-0">
+        <div className="flex flex-col min-h-screen">
+          <header className="h-16 px-6 flex items-center bg-gray-100 border-b-[0.5px] border-gray-300">
+            <LucideHome className="text-gray-500" />
+          </header>
+          <div className="flex flex-1">
+            <aside className="w-80 p-6 flex flex-col bg-gray-100 border-x-[0.5px] border-gray-300">
+              <div>Sidebar</div>
+              <div className="flex-1" />
+            </aside>
+            <div className="flex flex-col flex-1">
+              <main className="flex flex-col flex-1 p-6 pb-3">
+                <div className="bg-gray-900 text-white p-6 flex-1 rounded-xl">
+                  {children}
+                </div>
+              </main>
+              <div className="flex h-16 px-8">Bottom Navigation</div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
